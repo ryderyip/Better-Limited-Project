fe7c7a52ecc13f723bdc80389b7c88d179199cef
Anna <97609550+swattcchee@users.noreply.github.com>
2022-04-17 13:49:02 +0800

    ProductListForm + repository
    dataTable to list

--- Better Limited Project/ProductUtility/ProductList/ProductListForm.cs
+++ Better Limited Project/ProductUtility/ProductList/ProductListForm.cs
@@ -1,44 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Better_Limited_Project.ProductUtility.ProductList
 {
     public partial class ProductListForm : Form
     {
+        public delegate void ProductClickedEventHandler<T>(object sender, T t);
+
+      
+        
+
+
         public ProductListForm()
         {
             InitializeComponent();
+       
         }
+
+      
 
         private void ProductListForm_Load(object sender, EventArgs e)
         {
-            productBindingSource.Add(new Product()
-            {
-                ProductID = "S00001",
-                ProductName = "Item X.1",
-                Price = "Item X.2",
-                SellingPrice = "Item X.3",
-                Description = "Item X.4",
-                Supplier = "Item X.5"
-            });
 
-            productBindingSource.Add(new Product()
-            {
-                ProductID = "S00002",
-                ProductName = "Item X.1",
-                Price = "Item X.2",
-                SellingPrice = "Item X.3",
-                Description = "Item X.4",
-                Supplier = "Item X.5"
-            });
-
+            dgvProductList.DataSource = ProductRepository.GetProducts();
+               
+            
         }
 
         
